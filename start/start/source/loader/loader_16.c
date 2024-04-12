598b5a8f278d588deb9578446d276acf9645ee64
Haibo Sun <h3sun@uwaterloo.ca>
2024-04-12 02:18:54 -0400

    Print Msgs in C using Inline Asm

--- start/start/source/loader/loader_16.c
+++ start/start/source/loader/loader_16.c
@@ -2,6 +2,22 @@
 // Protected Mode
 __asm__(".code16gcc");
 
+
+// Inline Assembly
+// Used for Showing Messages with INT Software Interrupt
+static void show_msg(const char* msg){
+    char c;
+    while((c = *msg++) != '\0'){
+        __asm__ __volatile__(
+            "mov $0xe, %%ah\n\t"
+            "mov %[ch], %%al\n\t"
+            "int $0x10"::[ch]"r"(c)
+        );
+    }
+}
+
+
 void loader_entry(void) {
+    show_msg("....Loading....");
     for(;;) {}
 }
