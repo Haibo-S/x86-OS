05b5945ce999fa35fe85e4cbe457fafa18d6c2f1
Haibo Sun <h3sun@uwaterloo.ca>
2024-04-18 02:37:07 -0400

    reloading gdt complete

--- start/start/source/kernel/include/os_cfg.h
+++ start/start/source/kernel/include/os_cfg.h
@@ -1,9 +1,9 @@
-#ifndef OS_CFG_H
-#define OS_CFG_H
+#ifndef OS_OS_CFG_H
+#define OS_OS_CFG_H
 
-#define GDT_TABLE_SIZE      256
-#define KERNEL_SELECTOR_CS  (1 * 8)
-#define KERNEL_SELECTOR_DS  (2 * 8)
+#define GDT_TABLE_SIZE      	256		
+#define KERNEL_SELECTOR_CS		(1 * 8)		
+#define KERNEL_SELECTOR_DS		(2 * 8)		
+#define KERNEL_STACK_SIZE       (8*1024)   
 
-
-#endif
+#endif //OS_OS_CFG_H
--- start/start/source/kernel/init/init.c
+++ start/start/source/kernel/init/init.c
@@ -2,7 +2,12 @@
 #include "comm/boot_info.h"
 #include "cpu/cpu.h"
 
+void init_main(void){
+    for(;;){}
+}
+
+
 void kernel_init (boot_info_t * boot_info) {
     cpu_init();
-    for (;;) {}
+
 }
