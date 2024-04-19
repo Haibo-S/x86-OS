f28c6446f2eb45a770243a89f5e9fbf6103ed0ce
Haibo Sun <h3sun@uwaterloo.ca>
2024-04-19 03:30:50 -0400

    exception handler added

--- start/start/source/kernel/cpu/irq.c
+++ start/start/source/kernel/cpu/irq.c
@@ -9,6 +9,14 @@ void exception_handler_unknown(void);
 
 static gate_desc_t idt_table[IDE_TABLE_NR];
 
+static void do_default_handle(const char* message){
+    for(;;){}
+}
+
+void do_handler_unknown(void){
+    do_default_handle("unknown exception");
+}
+
 void irq_init(void){
     for(int i = 0; i < IDE_TABLE_NR; i++){
         gate_desc_set(idt_table + i, KERNEL_SELECTOR_CS, (uint32_t)exception_handler_unknown, 
