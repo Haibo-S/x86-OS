be7f9cd72396df86e6a463c67887676f184936c6
Haibo Sun <h3sun@uwaterloo.ca>
2024-04-29 23:12:15 -0400

    tss struct added

--- start/start/source/kernel/include/cpu/cpu.h
+++ start/start/source/kernel/include/cpu/cpu.h
@@ -21,7 +21,17 @@
 #define GATE_TYPE_IDT		(0xE << 8)		
 #define GATE_P_PRESENT		(1 << 15)		
 #define GATE_DPL0			(0 << 13)		
-#define GATE_DPL3			(3 << 13)		
+#define GATE_DPL3			(3 << 13)	
+
+typedef struct _tss_t {
+    uint32_t pre_link;
+    uint32_t esp0, ss0, esp1, ss1, esp2, ss2;
+    uint32_t cr3;
+    uint32_t eip, eflags, eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    uint32_t es, cs, ss, ds, fs, gs;
+    uint32_t ldt;
+    uint32_t iomap;
+}tss_t;
 
 #pragma pack(1)
 
