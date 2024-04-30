7f9c19dcb7ebd89c7a81629c09aff8997ac0c722
Haibo Sun <h3sun@uwaterloo.ca>
2024-04-29 23:38:57 -0400

    core task added

--- start/start/source/kernel/init/init.c
+++ start/start/source/kernel/init/init.c
@@ -6,6 +6,10 @@
 #include "tools/log.h"
 #include "os_cfg.h"
 #include "tools/klib.h"
+#include "core/task.h"
+
+static task_t first_task;
+static task_t init_task;
 
 void kernel_init (boot_info_t * boot_info) {
     cpu_init();
@@ -25,6 +29,9 @@ void init_main(void){
     log_printf("Kernel is Running.......");
     log_printf("Version: %s", OS_VERSION);
     log_printf("%d %d %x %c", 123, -123456, 0x12345, 'a');
+
+    task_init(&init_task, (uint32_t)init_task_entry, 0);
+    task_init(&first_task, 0, 0);
     
     int count = 0;
     for(;;){
