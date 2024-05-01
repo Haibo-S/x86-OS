9fd747dca78c706e00a1b9bfb3d3e14c17192f7b
Haibo Sun <h3sun@uwaterloo.ca>
2024-05-01 11:23:37 -0400

    listnode init

--- start/start/source/kernel/init/init.c
+++ start/start/source/kernel/init/init.c
@@ -1,3 +1,9 @@
+/**
+ * 内核初始化以及测试代码
+ *
+ * 作者：李述铜
+ * 联系邮箱: 527676163@qq.com
+ */
 #include "comm/boot_info.h"
 #include "comm/cpu_instr.h"
 #include "cpu/cpu.h"
@@ -7,6 +13,7 @@
 #include "core/task.h"
 #include "os_cfg.h"
 #include "tools/klib.h"
+#include "tools/list.h"
 
 static boot_info_t * init_boot_info;       
 
@@ -20,10 +27,9 @@ void kernel_init (boot_info_t * boot_info) {
     time_init();
 }
 
-static task_t first_task;       
-static uint32_t init_task_stack[1024];	
+static task_t first_task;   
+static uint32_t init_task_stack[1024];
 static task_t init_task;
-
 
 void init_task_entry(void) {
     int count = 0;
@@ -34,7 +40,65 @@ void init_task_entry(void) {
     }
 }
 
+void list_test (void) {
+    list_t list;
+    list_node_t nodes[5];
+    
+    list_init(&list);
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert first to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_first(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    list_init(&list);
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = list_remove_first(&list);
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("insert last to list: %d, 0x%x", i, (uint32_t)node);
+        list_insert_last(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    for (int i = 0; i < 5; i++) {
+        list_node_t * node = nodes + i;
+        log_printf("remove first from list: %d, 0x%x", i, (uint32_t)node);
+        list_remove(&list, node);
+    }
+    log_printf("list: first=0x%x, last=0x%x, count=%d", list_first(&list), list_last(&list), list_count(&list));
+
+    struct type_t {
+        int i;
+        list_node_t node;
+    }v = {0x123456};
+
+    list_node_t * v_node = &v.node;
+    struct type_t * p = list_node_parent(v_node, struct type_t, node);
+    if (p->i != 0x123456) {
+        log_printf("error");
+    }
+ }
+
 void init_main(void) {
+    list_test();
+
     log_printf("Kernel is running....");
     log_printf("Version: %s, name: %s", OS_VERSION, "tiny x86 os");
     log_printf("%d %d %x %c", -123, 123456, 0x12345, 'a');
@@ -44,7 +108,6 @@ void init_main(void) {
     write_tr(first_task.tss_sel);
 
     //int a = 3 / 0;
-    // irq_enable_global();
     int count = 0;
     for (;;) {
         log_printf("first task: %d", count++);
